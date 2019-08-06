aab394a10764361819e8353faea3974123837ca8
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2019-08-06 10:38:09 -0600

    create datastore

---
+++ datastore.js
@@ -0,0 +1,111 @@
+const STORE = [
+    {
+      question: 'What is the term for a score one over par?',
+      answers: [
+        'Par',
+        'Birdie',
+        'Eagle',
+        'Bogey'
+      ],
+      correctAnswer: 'Bogey'
+    },
+    {
+     question: 'What is the penalty for hitting the ball out of bounds?',
+      answers: [
+        'No Penalty',
+        'Disqualification',
+        '1 Stroke Penalty',
+        '2 Stroke Penalty',
+      ],
+      correctAnswer: '1 Stroke Penalty'
+    },
+    {
+      question: 'What is the difference between hazards marked by red and white stakes?',
+      answers: [
+        'There is no difference.',
+        'Red hazards you must hit from the spot you last hit and add a stroke. White hazards you drop your ball as close to where the ball entered the hazard',
+        'White hazards you must hit from the spot you last hit and add a stroke. Red hazards you drop your ball as close to where the ball entered the hazard',
+        'You must drop where you last hit for both'
+      ],
+      correctAnswer: 'White hazards you must hit from the spot you last hit and add a stroke. Red hazards you drop your ball as close to where the ball entered the hazard'
+    },
+  
+    {
+      question: 'When on the green, who hits first?',
+      answers: [
+        'Whoever is ready first',
+        'The closest to the pin',
+        'The farthest from the pin',
+        'It doesn’t matter'
+      ],
+      correctAnswer: 'The farthest from the pin',
+    },
+  
+    {
+      question: 'How many clubs are you allowed to have in your bag?',
+      answers: [
+        '10',
+        '12',
+        '14',
+        '8',
+      ],
+      correctAnswer: '14',
+    },
+  
+    {
+      question: 'What does a red flag pin mean',
+      answers: [
+        'The pin is on the right',
+        'The pin is on the back of the green',
+        'The pin is on the back of the green',
+        'The pin is on the front of the green',
+      ],
+      correctAnswer: 'The pin is on the front of the green',
+    },
+  
+    {
+      question: 'If a right handed golfer hits the ball and it tails left to right, what is it called?',
+      answers: [
+        'A fade or slice',
+        'A draw or hook',
+        'Nothing',
+        'A duff',
+      ],
+      correctAnswer: 'A fade or slice',
+    },
+  
+    {
+      question: 'If your ball is on the green and another person hits your ball with their ball, what happens?',
+      answers: [
+        'You both receive 1 penalty stroke',
+        'The other player receives a 2 stroke penalty.',
+        'The other player is disqualified',
+        'You receive a 2 stroke penalty',
+      ],
+      correctAnswer: 'You receive a 2 stroke penalty',
+    },
+  
+    {
+      question: 'You score a 3 on a par 5, what is the correct term to describe your score?',
+      answers: [
+        'Par',
+        'Eagle',
+        'Birdie',
+        'Bogey',
+      ],
+      correctAnswer: 'Eagle',
+    },
+  
+   {
+      question: 'Can you take a practice swing inside of a bunker?',
+      answers: [
+        'Yes, as long as you don’t ground your club',
+        'No, you must swing outside of the bunker',
+        'Yes, you can ground your club in the sand',
+        'Yes, you can ground your club in fairway bunkers but not by the green.',
+      ],
+      correctAnswer: 'Yes, as long as you don’t ground your club',
+    }  
+  ]
+  
+  
